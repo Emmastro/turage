c07a88ee1de934451d71c63086592544637b737b
Virgo Alpha <bensonkingori96@gmail.com>
2022-08-11 13:22:15 +0300

    Passenger Ride Request

--- ride/urls.py
+++ ride/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path("ride/request/", views.RideRequestView.as_view(), name="ride-request"),
     path("ride/request-near/", views.RideRequestNearView.as_view(), name="ride-request-near"),
+    path("ride/passenger-request", views.PassengerRideRequestView.as_view(), name="Passenger-ride-request"),
     path('', views.home, name='home'),
 ]
