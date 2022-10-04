d976f839b4b525237b0306b02a672542c088f14a
Emmanuel Murairi <emurairi18@alastudents.org>
2022-10-04 21:52:04 +0400

    load test data from spreadsheet

--- ride/management/commands/demo_data.py
+++ ride/management/commands/demo_data.py
@@ -1,45 +1,57 @@
 from django.core.management.base import BaseCommand
-from ride.models import Passenger, Driver, RideRequest, RideRequestMatched, Waypoint
+from ride.models import Car, Edge, Passenger, Driver, RideRequest, RideRequestMatched, Waypoint
 from django.db.utils import IntegrityError
 
+import pandas as pd
+
+from django.conf import settings
+
 class Command(BaseCommand):
+    path = settings.BASE_DIR 
+    spreadsheet = path + '/ride/test_data/data_valid.xlsx'
+    print(spreadsheet)
+    mapping = [
+        ('cars', Car),
+        ('drivers', Driver),
+        ('edges', Edge),
+        ('passengers', Passenger),
+    ]
 
     def handle(self, *args, **options):
 
         # Create test waypoints
 
-        
-
         # Create test passengers and drivers
 
-        for i in range(5):
-            print(f"loop {i}")
+        self.load_spreadsheet_test_data()
+        # for i in range(5):
+        #     print(f"loop {i}")
 
-            passenger = self.demo_passengers(i)
-            driver = self.demo_driver(i)
-            waypoint = Waypoint.objects.create(
-                name=f"Waypoint {i}",
-                #address=f"Address {i}",
-                latitude=0,
-                longitude=0,
-            )
-            waypoint.save()
-            #ride_request = self.demo_ride_request(i, passenger, waypoint)
-            #self.demo_ride_request_matched(i, driver, ride_request)
-            # create test ride request
-            ride_request, ride_request_created = RideRequest.objects.get_or_create(
-                number_passengers=1, passenger=passenger, driver=driver,
-                distance=4)
-            #ride_request.save()
+        #     passenger = self.demo_passengers(i)
+        #     driver = self.demo_driver(i)
+        #     waypoint = Waypoint.objects.create(
+        #         name=f"Waypoint {i}",
+        #         #address=f"Address {i}",
+        #         latitude=0,
+        #         longitude=0,
+        #     )
+        #     waypoint.save()
+        #     #ride_request = self.demo_ride_request(i, passenger, waypoint)
+        #     #self.demo_ride_request_matched(i, driver, ride_request)
+        #     # create test ride request
+        #     ride_request, ride_request_created = RideRequest.objects.get_or_create(
+        #         number_passengers=1, passenger=passenger, driver=driver,
+        #         distance=4)
+        #     #ride_request.save()
 
-            # Match request 1 and 2
-            if i == 1:
+        #     # Match request 1 and 2
+        #     if i == 1:
 
-                match = RideRequestMatched()
-                match.save() # save to generate an id
-                match.matches.add(ride_request)
-                match.matches.add(RideRequest.objects.get(id=1))
-                match.save()
+        #         match = RideRequestMatched()
+        #         match.save() # save to generate an id
+        #         match.matches.add(ride_request)
+        #         match.matches.add(RideRequest.objects.get(id=1))
+        #         match.save()
 
     def demo_driver(self, i):
         try:
@@ -58,3 +70,24 @@ class Command(BaseCommand):
         except IntegrityError as e:
             print(f"Passenger{i} already exists")
         return passenger
+
+    def load_spreadsheet_test_data(self):
+        """
+        Tests data for each model can be loaded as expected, 
+        and wrong data input returns that appropriate error based on the test cases 
+        from the test spreadsheet.
+        """
+
+        for (sheet_name, model) in self.mapping:
+
+            data = pd.read_excel(self.spreadsheet, sheet_name=sheet_name)
+            data.fillna('', inplace=True)
+
+            if sheet_name == 'drivers':
+                cars = data['car']
+                data.drop(columns=['car'], inplace=True)
+            for row in data.to_dict('records'):
+                response_code = row.pop('response_code')
+                
+                instance = model.objects.create(**row)
+                instance.save()
